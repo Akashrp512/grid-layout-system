7d07d73bd2e240c909117d3355e406f4bbba6297
Akash RP <95461248+Akashrp512@users.noreply.github.com>
2022-10-14 20:04:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div class="grid-container">
+      <div class="grid-item" id="item1">
+        1
+      </div>
+      <div class="grid-item" id="item2">
+        2
+      </div>
+      <div class="grid-item" id="item3">
+        3
+      </div>
+      <div class="grid-item" id="item4">
+        4
+      </div>
+      <div class="grid-item" id="item5">
+        5
+      </div>
+      <div class="grid-item" id="item6">
+        6
+      </div>
+      <div class="grid-item" id="item7">
+        7
+      </div>
+      <div class="grid-item" id="item8">
+        8
+      </div>
+      <div class="grid-item" id="item9">
+        9
+      </div>
+    </div>
+  );
+}
